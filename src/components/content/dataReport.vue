d8cdbd8d5c408610d64fe5479a0cefc6fa575c6e
Maple Ran <maple.ranwei@gmail.com>
2017-11-27 18:21:51 +0800

    bug fix

--- src/components/content/dataReport.vue
+++ src/components/content/dataReport.vue
@@ -2,12 +2,27 @@
   <div id="content">
     <div class="row" v-cloak>
       <div class="col-sm-12">
-        <div class="ibox float-e-margins" style="margin-bottom: 0px">
-          <div class="ibox-title">
-            违章指数
+        <div class="ibox float-e-margins" style="margin-bottom: 10px">
+          <div class="ibox-content">
+            <div id="car" style="width: 1070px; height: 340px;"></div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" v-cloak>
+      <div class="col-sm-12">
+        <div class="ibox float-e-margins" style="margin-bottom: 10px">
           <div class="ibox-content">
             <div id="ticket" style="width: 1070px; height: 340px;"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" v-cloak>
+      <div class="col-sm-12">
+        <div class="ibox float-e-margins" style="margin-bottom: 0px">
+          <div class="ibox-content">
+            <div id="payment" style="width: 1070px; height: 240px;"></div>
           </div>
         </div>
       </div>
@@ -18,14 +33,23 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   var echarts = require('echarts')
+  require('../../../static/js/theme/roma')
+  require('../../../static/js/theme/vintage')
   export default {
+    data () {
+      return {}
+    },
     mounted () {
+      this.initCarChart(2017, 0)
       this.initTicketChart()
+      this.initPaymentChart()
     },
     methods: {
-      initTicketChart () {
-        var ticketChart = echarts.init(document.getElementById('ticket'))
+      initCarChart (year, branch) {
+        var ticketChart = echarts.init(document.getElementById('car'), 'vintage')
         ticketChart.setOption({
           tooltip: {
             trigger: 'axis',
@@ -36,19 +60,220 @@
               }
             }
           },
+          title: {
+            text: '车辆指标',
+            x: 'center'
+          },
           legend: {
-            data: ['个人现有', '个人新增', '总计指数', '平均指数']
+            x: 'center',
+            y: 'bottom',
+            data: ['车辆总数', '当月新增']
           },
           grid: {
             left: '1%',
             right: '1%',
-            bottom: '1%',
+            bottom: '10%',
             containLabel: true
           },
           xAxis: [
             {
               type: 'category',
-              data: ['11日', '12日', '13日', '14日', '15日', '16日', '17日', '18日', '19日', '20日', '21日', '22日'],
+              data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+              axisPointer: {
+                type: 'shadow'
+              }
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value',
+              name: '现有车辆',
+              axisLabel: {
+                formatter: '{value}'
+              }
+            },
+            {
+              type: 'value',
+              name: '新增车辆',
+              axisLabel: {
+                formatter: '{value}'
+              }
+            }
+          ],
+          series: [
+            {
+              name: '车辆总数',
+              type: 'bar',
+              data: []
+            },
+            {
+              name: '每月新增',
+              type: 'line',
+              yAxisIndex: 1,
+              data: []
+            }
+
+          ]
+
+        })
+        // 获取数据
+        axios.get('/api/data_report/car.do', {
+          params: {
+            year: year,
+            branch: branch
+          }
+        }).then((res) => {
+          ticketChart.setOption({
+            series: [
+              {
+                name: '车辆总数',
+                data: res.data.data.total
+              },
+              {
+                name: '当月新增',
+                data: res.data.data.add
+              }
+            ]
+          })
+        })
+      },
+      initPaymentChart () {
+        var paymentChart = echarts.init(document.getElementById('payment'), 'vintage')
+        var labelTop = {
+          normal: {
+            label: {
+              show: true,
+              position: 'center',
+              formatter: '{b}',
+              textStyle: {
+                baseline: 'bottom'
+              }
+            },
+            labelLine: {
+              show: false
+            }
+          }
+        }
+        var labelFromatter = {
+          normal: {
+            label: {
+              formatter: function (params) {
+                return 100 - params.value + '%'
+              },
+              textStyle: {
+                baseline: 'top',
+                color: 'rgb(254,67,101)'
+              }
+            }
+          }
+        }
+        var labelBottom = {
+          normal: {
+            color: '#ccc',
+            label: {
+              show: true,
+              position: 'center'
+            },
+            labelLine: {
+              show: false
+            }
+          },
+          emphasis: {
+            color: 'rgba(0,0,0,0)'
+          }
+        }
+        var radius = [40, 55]
+        paymentChart.setOption({
+          legend: {
+            x: 'center',
+            y: 'bottom',
+            data: [
+              '周还款率:金额', '周还款率:人数', '月还款率:金额', '月还款率:人数'
+            ]
+          },
+          title: {
+            text: '还款率',
+            x: 'center'
+          },
+          series: [
+            {
+              type: 'pie',
+              center: ['15%', '50%'],
+              radius: radius,
+              x: '0%', // for funnel
+              itemStyle: labelFromatter,
+              data: [
+                {name: 'other', value: 46, itemStyle: labelBottom},
+                {name: '周还款率:金额', value: 54, itemStyle: labelTop}
+              ]
+            },
+            {
+              type: 'pie',
+              center: ['35%', '50%'],
+              radius: radius,
+              x: '20%', // for funnel
+              itemStyle: labelFromatter,
+              data: [
+                {name: 'other', value: 56, itemStyle: labelBottom},
+                {name: '周还款率:人数', value: 44, itemStyle: labelTop}
+              ]
+            },
+            {
+              type: 'pie',
+              center: ['65%', '50%'],
+              radius: radius,
+              x: '40%', // for funnel
+              itemStyle: labelFromatter,
+              data: [
+                {name: 'other', value: 65, itemStyle: labelBottom},
+                {name: '月还款率:金额', value: 35, itemStyle: labelTop}
+              ]
+            },
+            {
+              type: 'pie',
+              center: ['85%', '50%'],
+              radius: radius,
+              x: '60%', // for funnel
+              itemStyle: labelFromatter,
+              data: [
+                {name: 'other', value: 40, itemStyle: labelBottom},
+                {name: '月还款率:人数', value: 60, itemStyle: labelTop}
+              ]
+            }
+          ]
+        })
+      },
+      initTicketChart () {
+        var ticketChart = echarts.init(document.getElementById('ticket'), 'vintage')
+        ticketChart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              crossStyle: {
+                color: '#999'
+              }
+            }
+          },
+          title: {
+            text: '违章指数',
+            x: 'center'
+          },
+          legend: {
+            x: 'center',
+            y: 'bottom',
+            data: ['总计指数', '新增指数']
+          },
+          grid: {
+            left: '1%',
+            right: '1%',
+            bottom: '10%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: [],
               axisPointer: {
                 type: 'shadow'
               }
@@ -58,9 +283,6 @@
             {
               type: 'value',
               name: '金额',
-              min: 0,
-              max: 20000,
-              interval: 4000,
               axisLabel: {
                 formatter: '{value} 元'
               }
@@ -68,9 +290,6 @@
             {
               type: 'value',
               name: '金额',
-              min: 0,
-              max: 20000,
-              interval: 4000,
               axisLabel: {
                 formatter: '{value} 元'
               }
@@ -78,29 +297,36 @@
           ],
           series: [
             {
-              name: '个人现有',
-              type: 'bar',
-              data: [2, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
-            {
-              name: '个人新增',
-              type: 'bar',
-              data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-            },
-            {
-              name: '平均指数',
+              name: '新增指数',
               type: 'line',
               yAxisIndex: 1,
-              data: [2.3, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+              data: []
             },
             {
               name: '总计指数',
-              type: 'line',
-              yAxisIndex: 1,
-              data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 21.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+              type: 'bar',
+              yAxisIndex: 0,
+              data: []
             }
 
           ]
+        })
+        axios.get('/api/data_report/ticket.do').then((res) => {
+          ticketChart.setOption({
+            xAxis: {
+              data: res.data.data.date
+            },
+            series: [
+              {
+                name: '总计指数',
+                data: res.data.data.total
+              },
+              {
+                name: '新增指数',
+                data: res.data.data.add
+              }
+            ]
+          })
         })
       }
     }
